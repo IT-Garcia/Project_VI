4d2522132a7b12c67ade342d0529814a433a797d
Isai Torres Garcia <isai.torresga@gmail.com>
2024-08-09 14:02:46 -0400

    Old files removed

--- php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	// logout.php
-	session_start(); // Required for every page where you call or declare a session
-	session_destroy(); // Destroys all data registered to a session
-	header('Location: ../login.html'); // Redirect to login page
-	exit();
-?>
-
-
